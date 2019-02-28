32911044ff829699f8e8e2ba3939bd5830157596
Héctor López <hector.lopez@DSA-DEV2-10>
2019-02-27 18:52:24 -0600

    add method in todo component

--- task-angular-firebase/src/app/components/todo/todo.component.ts
+++ task-angular-firebase/src/app/components/todo/todo.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { TodoService } from '../../services/todo.service';
 
 @Component({
   selector: 'app-todo',
@@ -7,9 +8,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TodoComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private _todoService: TodoService
+  ) { }
 
   ngOnInit() {
+  }
+
+  addTodo(itemTitle){
+    //this._todoService.addTodo(itemTitle);
+    console.log('Agregando....')
   }
 
 }
