6a37d343a999332bef349dc7fb49201de12b244e
Héctor López <hector.lopez@DSA-DEV2-10>
2019-03-13 12:30:21 -0600

    try query

--- task-angular-firebase/src/app/services/todo.service.ts
+++ task-angular-firebase/src/app/services/todo.service.ts
@@ -1,19 +1,46 @@
 import { Injectable } from '@angular/core';
-import { AngularFireDatabase, AngularFireList, QueryFn, AngularFireAction } from 'angularfire2/database';
+import { AngularFireDatabase, AngularFireList, QueryFn, AngularFireAction, AngularFireDatabaseModule, snapshotChanges } from 'angularfire2/database';
 import { TodoModel } from '../models/todo';
+import { environment } from '../../environments/environment'
+import * as firebase from 'firebase';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TodoService {
   todoList: AngularFireList<any>;
+  listx: TodoModel[];
   selectedTask_temporally: TodoModel = new TodoModel();
 
   constructor(
     private firebasedb: AngularFireDatabase,
   ) {}
 
+  getTodoListDate(){
+    
+    var f = firebase.firestore()
+    //var app = firebase.initializeApp(environment.firebase)
+    //var db = firebase.firestore(app)
+    var col = f.collection('tasks')
+    var query = col.where("completed", "==", false).orderBy('dateCreate').onSnapshot((
+      snapshot: firebase.firestore.QuerySnapshot) =>{
+      const list = [];
+      snapshot.forEach(doc =>        
+        list.push(Object.assign({},
+          doc.data(),
+          {
+            id: doc.id,
+          }))
+      );
+      this.listx = list;
+    });
+    console.log('__LISTX')
+    console.log(this.listx)
+    
+  }
+
   getTodoList(){
+    this.getTodoListDate()
     this.todoList = this.firebasedb.list('tasks');
     return this.todoList;
   }
